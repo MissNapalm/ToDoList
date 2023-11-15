7243fe568a1d9e86f73c9afb13981784f2b5686f
MissNapalm <148738331+MissNapalm@users.noreply.github.com>
2023-11-14 21:56:41 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,53 +1,81 @@
-// Function to add a new task to the list
-function addTask() {
-    var taskInput = document.getElementById("taskInput");
-    var taskValue = taskInput.value;
+var selectedRow = null
 
-    if (taskValue.trim() === "") {
-        alert("Please enter a task!");
-        return;
-    }
-
-    var listItem = createTaskElement(taskValue);
-    var taskList = document.getElementById("taskList");
-    taskList.appendChild(listItem);
-
-    taskInput.value = "";
-}
-
-// Function to create a new task element
-function createTaskElement(taskValue) {
-    var listItem = document.createElement("li");
-    listItem.textContent = taskValue;
-
-    var editButton = document.createElement("button");
-    editButton.textContent = "Edit";
-    editButton.onclick = function () {
-        editTask(listItem);
-    };
-
-    var deleteButton = document.createElement("button");
-    deleteButton.textContent = "Delete";
-    deleteButton.onclick = function () {
-        deleteTask(listItem);
-    };
-
-    listItem.appendChild(editButton);
-    listItem.appendChild(deleteButton);
-
-    return listItem;
-}
-
-// Function to edit a task
-function editTask(listItem) {
-    var newTaskValue = prompt("Edit task:", listItem.textContent.trim());
-
-    if (newTaskValue !== null) {
-        listItem.textContent = newTaskValue;
+function onFormSubmit() {
+    if (validate()) {
+        var formData = readFormData();
+        if (selectedRow == null)
+            insertNewRecord(formData);
+        else
+            updateRecord(formData);
+        resetForm();
     }
 }
 
-// Function to delete a task
-function deleteTask(listItem) {
-    listItem.remove();
+function readFormData() {
+    var formData = {};
+    formData["fullName"] = document.getElementById("fullName").value;
+    formData["email"] = document.getElementById("email").value;
+    formData["salary"] = document.getElementById("salary").value;
+    formData["city"] = document.getElementById("city").value;
+    return formData;
+}
+
+function insertNewRecord(data) {
+    var table = document.getElementById("employeeList").getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.length);
+    cell1 = newRow.insertCell(0);
+    cell1.innerHTML = data.fullName;
+    cell2 = newRow.insertCell(1);
+    cell2.innerHTML = data.email;
+    cell3 = newRow.insertCell(2);
+    cell3.innerHTML = data.salary;
+    cell4 = newRow.insertCell(3);
+    cell4.innerHTML = data.city;
+    cell4 = newRow.insertCell(4);
+    cell4.innerHTML = `<a onClick="onEdit(this)">Edit</a>
+                       <a onClick="onDelete(this)">Delete</a>`;
+}
+
+function resetForm() {
+    document.getElementById("fullName").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("salary").value = "";
+    document.getElementById("city").value = "";
+    selectedRow = null;
+}
+
+function onEdit(td) {
+    selectedRow = td.parentElement.parentElement;
+    document.getElementById("fullName").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("email").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("salary").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("city").value = selectedRow.cells[3].innerHTML;
+}
+
+function updateRecord(formData) {
+    selectedRow.cells[0].innerHTML = formData.fullName;
+    selectedRow.cells[1].innerHTML = formData.email;
+    selectedRow.cells[2].innerHTML = formData.salary;
+    selectedRow.cells[3].innerHTML = formData.city;
+}
+
+function onDelete(td) {
+    if (confirm('Are you sure you want to delete this record ?')) {
+        row = td.parentElement.parentElement;
+        document.getElementById("employeeList").deleteRow(row.rowIndex);
+        resetForm();
+    }
+}
+
+function validate(){
+    isValid = true;
+    if (document.getElementById("fullName").value == ""){
+        isValid = false;
+        document.getElementById("fullNameValidationError").classList.remove("hide");
+    } else {
+        isValid = true;
+        if (!document.getElementById("fullNameValidationError").classList.contains("hide"))
+        document.getElementById("fullNameValidationError").classList.add("hide");
+    }
+    return isValid;
 }
